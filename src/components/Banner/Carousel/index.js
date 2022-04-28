6ca9bf4dd92037ef106d01817d1f5e51a3a92f23
Diego Rocha <dkxever@gmail.com>
2022-04-28 17:28:36 -0300

    Items and responsive of Alice Carousel

--- src/components/Banner/Carousel/index.js
+++ src/components/Banner/Carousel/index.js
@@ -30,6 +30,60 @@ export const Carousel = () => {
     fetchTrendingCoin();
   },[currency]);
 
+  // Meus items do Alice Carousel
+
+  const items = trending.map((coin) => {
+
+    const profit = coin?.price_change_percentage_24h >= 0;
+
+    return(
+      <Link
+        to={`coins/${coin.id}`}
+        className={classes.carouselItem}
+      >
+        <img
+          src={coin?.image}
+          alt={coin.name}
+          height={80}
+          style={{ marginBottom: 20 }}
+        />
+        <span>
+          {coin?.symbol}
+          &nbsp;
+          <span
+            style={{
+              color: profit > 0 ? "rgb(14, 203, 129)" : "red",
+              fontWeight: 500
+            }}
+          >
+            {profit && "+"}
+            {coin?.price_change_percentage_24h?.toFixed(2)}%
+          </span>
+        </span>
+        <span
+          style={{
+            fontSize: 22,
+            fontWeight: 500
+          }}
+        >
+          {symbol}{" "}
+          {numberWithCommas(coin?.current_price.toFixed(2))}
+        </span>
+      </Link>
+    )
+  });
+
+  // Reponsive do Alice Carousel
+   
+  const responsive = {
+    0:{
+      items: 2
+    },
+    512:{
+      items: 4
+    }
+  };
+
   return(
     <div>
       ...
