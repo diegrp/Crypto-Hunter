4e68c0a15164035745856e97595d07b2f74825a5
Diego Rocha <dkxever@gmail.com>
2022-04-28 18:43:13 -0300

    Styling our CoinInfo

---
+++ src/components/CoinInfo/styles.js
@@ -0,0 +1,27 @@
+import { makeStyles, createTheme } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles((theme) => ({
+  container:{
+    width: "75%",
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 25,
+    padding: 40,
+    [theme.breakpoints.down("md")]:{
+      width: "100%",
+      marginTop: 0,
+      padding: 20
+    }
+  }
+}));
+
+export const darkTheme = createTheme({
+  palette:{
+    primary:{
+      main: "#fff"
+    },
+    type: "dark"
+  }
+});
