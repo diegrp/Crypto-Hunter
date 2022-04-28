91697bb5c70b77cf2410a6815a0f1a2ddf9d337b
Diego Rocha <dkxever@gmail.com>
2022-04-28 17:19:00 -0300

    Structuring and organizing our Header with MUI Component and auth components

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -23,8 +23,35 @@ export const Header = () => {
   const navigate = useNavigate();
 
   return(
-    <div>
-      ...
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <AppBar color="transparent" position="static">
+        <Container>
+          <Toolbar>
+            {/* Title Logo */}
+            <Typography
+              variant="h5"
+              onClick={() => navigate("/")}
+              className={classes.title}
+            > 
+              Crypto Hunter
+            </Typography> 
+            {/* Select Crypto Currency */}
+            <Select
+              style={{
+                width: 100,
+                height: 40,
+                marginRight: 15
+              }}
+              value={currency}
+              onChange={({target}) => setCurrency(target.value)} 
+            >
+              <MenuItem value={"INR"}>INR</MenuItem>
+              <MenuItem value={"USD"}>USD</MenuItem>
+            </Select>
+            { user ? <UserSidebar/> : <AuthModal/> } 
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </ThemeProvider>
   )
 }
