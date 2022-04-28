b890646b75b0a7656c179e5df01b8746df2bfc1c
Diego Rocha <dkxever@gmail.com>
2022-04-28 19:19:02 -0300

    Structuring and organizing our UserSidebar with MUI Component and watchlist

--- src/components/Authentication/UserSidebar/index.js
+++ src/components/Authentication/UserSidebar/index.js
@@ -78,7 +78,80 @@ export const UserSidebar = () => {
 
   return (
     <div>
-      ...
+      {["right"].map((anchor) => (
+        <React.Fragment key={anchor}>
+          <Avatar
+            onClick={toggleDrawer(anchor, true)}
+            style={{
+              width: 38,
+              height: 38,
+              backgroundColor: "#EEBC1D",
+              marginLeft: 15,
+              cursor: "pointer",
+            }}
+            src={user.photoURL}
+            alt={user.displayName || user.email}
+          />
+          <Drawer 
+            anchor={anchor} 
+            open={state[anchor]} 
+            onClose={toggleDrawer(anchor, false)}
+          >
+            <div className={classes.container}>
+              <div className={classes.profile}>
+                <Avatar
+                  className={classes.picture}
+                  src={user.photoURL}
+                  alt={user.displayName || user.email}
+                />
+                <span
+                  style={{
+                    width: "100%",
+                    fontSize: 25,
+                    fontWeight: "bold",
+                    textAlign: "center",
+                    wordBreak: "break-word"
+                  }}
+                >
+                  {user.displayName || user.email}
+                </span>
+                <div className={classes.watchlist}>
+                  <span style={{ fontSize: 15, textShadow: "0 0 5px black" }}>
+                    Lista de Favoritos
+                  </span>
+                  {coins.map((coin) => {
+                    if(watchlist.includes(coin?.id)){
+                      return(
+                        <div key={coin.id} className={classes.coin}>
+                          <span>{coin.name}</span>
+                          <span style={{ display: "flex", gap: 8 }}>
+                            {symbol}
+                            {numberWithCommas(coin.current_price.toFixed(2))}
+                            {/* Deletar item adicionado na minha lista */}
+                            <AiFillDelete
+                              size={16}
+                              style={{ cursor: "pointer" }}
+                              onClick={() => removeFromWatchlist(coin)}
+                            />
+                          </span>
+                        </div>
+                      )
+                    }
+                  })}
+                </div>
+              </div>
+              {/* Deslogar do meu sidebar */}
+              <Button
+                variant="contained"
+                className={classes.logout}
+                onClick={logout}
+              >
+                Sair
+              </Button>
+            </div>
+          </Drawer>
+        </React.Fragment>
+      ))}
     </div>
   );
 }
