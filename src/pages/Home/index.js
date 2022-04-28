aaf436ae6a69af7033421e810b3977a6c8e375d0
Diego Rocha <dkxever@gmail.com>
2022-04-28 18:57:47 -0300

    Structuring and organizing our Home with Banner and CoinsTable

---
+++ src/pages/Home/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Banner } from "../../components/Banner";
+import { CoinsTable } from "../../components/CoinsTable";
+
+// Home
+
+export const Home = () => {
+  return(
+    <>
+      <Banner/>
+      <CoinsTable/>
+    </>
+  )
+}
