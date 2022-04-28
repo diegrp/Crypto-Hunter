ecb66fa7b690708e2e17b7850801d227bed73f02
Diego Rocha <dkxever@gmail.com>
2022-04-28 18:33:18 -0300

    Passing and arranging the props for our SelectButton

---
+++ src/components/SelectButton/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { useStyles } from "./styles";
+
+// SelectButton
+
+export const SelectButton = ({ onClick, selected, children }) => {
+  
+  const classes = useStyles(selected);
+  
+  return(
+    <div>
+      ...
+    </div>
+  )
+}
