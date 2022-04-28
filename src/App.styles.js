2c6e59dba1372126e72fd8c945b1b3c8d402367c
Diego Rocha <dkxever@gmail.com>
2022-04-28 17:09:37 -0300

    Styling our App

---
+++ src/App.styles.js
@@ -0,0 +1,10 @@
+import { makeStyles } from "@material-ui/core/styles";
+
+export const useStyles = makeStyles(() => ({
+  App:{
+    fontFamily: "Montserrat",
+    backgroundColor: "#14161a",
+    minHeight: "100vh",
+    color: "#fff"
+  }
+}));
